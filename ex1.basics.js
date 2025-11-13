2d186e8498ab43a2ff2f370be729c5fdffc37ac0
topsoldier1aj-max <topsoldier1aj@gmail.com>
2025-11-13 08:57:01 +0200

    wk3 exercise 1

---
+++ ex1.basics.js
@@ -0,0 +1,9 @@
+const firstname = Abdul-Jaleel;
+const lastname = Mangala;
+let age = 19;
+
+let fullname =firstname + lastname;
+
+console.log("hi there, my name is" + fullname + "I am" + age +"years old.");
+
+age = 24;
